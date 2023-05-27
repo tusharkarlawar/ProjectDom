a2203207034f04ef66823f8c909771d4df6479b0
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-05-27 17:31:50 +0530

    //QUERYSELECTOR//

--- dom.js
+++ dom.js
@@ -1,5 +1,33 @@
-console.dir(document);
-//console.dir(document);
-//console.log(document.url);
+ //QUERYSELECTOR//
+   /* var header = document.querySelector("#main-header");
+    header.style.borderBottom = 'solid 4px #ccc';
 
-console.log(document.form);
+    var submit = document.querySelector('input[type="submit"]');
+    submit.value = 'SEND';
+
+
+    var item = document.querySelector(".list-group-item");
+    item.style.color = 'red'
+
+    var lastitem = document.querySelector(".list-group-item:last-child");
+    lastitem.style.color = 'blue'  */
+
+    var Seconditem = document.querySelector(".list-group-item:nth-child(2)");
+    Seconditem.style.color = 'green'
+
+    //QUERYSELECTORALL//
+    /*var titles = document.querySelectorAll('.title');
+    titles[0].textContent ='Hello';  */
+
+    var odd = document.querySelectorAll('li:nth-child(odd)');
+    for(var i=0; i<odd.length; i++)
+    {
+        odd[i].style.backgroundColor = 'green';
+    }
+
+
+   /* var even = document.querySelectorAll('li:nth-child(even)');
+    for(var i=0; i<even.length; i++)
+    {
+        even[i].style.backgroundColor = '#ccc';
+    }   */
